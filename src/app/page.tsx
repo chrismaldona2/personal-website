e7299a7556f20e6a5247e5e03a50e33131b1858e
Christian Maldonado <chris.ariel.maldonado@gmail.com>
2025-01-30 09:02:19 -0300

    experimenting with next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import WavingHand from "@/components/animations/WavingHand/WavingHand";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="min-h-screen text-white grid grid-cols-2 gap-8 items-start w-[80%] m-auto py-32">
+      <div className="text-2xl cursor-default flex flex-col gap-1 items-start ">
+        <span className="flex gap-2 items-center">
+          <WavingHand />
+          Hi I&apos;m
+        </span>
+        <h1 className="text-7xl/[100%] font-bold text-white">Christian</h1>
+        <span className="text-neutral-300 ml-1">Full-stack Developer</span>
+      </div>
+      <div>
+        <p className="text-xl">
+          I’m a computer engineer student with a designer’s heart, obsessed with
+          crafting pixel-perfect interfaces and breathing life into the web
+          through motion.
+        </p>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
